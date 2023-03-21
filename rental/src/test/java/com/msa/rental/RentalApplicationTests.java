0882d2f9f7cdfe42f751e5d091ceb3709f4b7b21
n4oah <n4oahdev@gmail.com>
2023-03-22 00:24:51 +0900

    init rental service

---
+++ rental/src/test/java/com/msa/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
